d6962a8bfb36bdab369906f4573fc8f47c5715df
Kevin Calle <kcalle012@gmail.com>
2025-08-17 09:50:44 -0400

    created project and added components

---
+++ src/Components/NavBar/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import "./index.scss"
+
+function NavBar() {
+  return (
+    <nav>
+        <div>hello</div>
+        <div>world</div>
+        <div>hi</div>
+    </nav>
+  )
+}
+
+export default NavBar
