8c317ef915ee81c43163f3d35c9307509bd8540f
Kevin Calle <kcalle012@gmail.com>
2025-11-11 14:39:39 -0500

    added prefilled subject line for email

--- src/Components/Contact/index.js
+++ src/Components/Contact/index.js
@@ -7,7 +7,7 @@ function Contact() {
       <h1>Contact: </h1>
       <p>
         You can direct any questions to the{" "}
-        <a href="mailto:roboticsclub@sgamsu.org"> Robotics Club email </a> or the
+        <a href="mailto:roboticsclub@sgamsu.org?subject=Question about the MicroMouse Hackathon"> Robotics Club email </a> or the
         <a href="mailto:computingclub@sgamsu.org"> Computing Club email</a>
       </p>
     </section>
